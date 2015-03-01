746c6470804ea9cb8c697334413dd94875e0783b
anil <anil.bhaila.2020@gmail.com>
2015-03-01 10:35:52 +0545

    'first'

---
+++ newsPortalPlugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+  Plugin Name: News Portal Plugin
+  Plugin URI: http://anilbhaila.com.np
+  Description: Updates user rating based on number of posts.
+  Version: 1.0
+  Author: Anil Bhaila
+  Author URI: http://anilbhaila.com.np
+ */
